295fc96fb5cac4048021646b969ae4892da4ed29
Ayopee <Ayopee001@gmail.com>
2026-01-21 18:09:47 +0700

    update port3005

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/test2",
-  assetPrefix: "/test2/",
+  assetPrefix: "/test2",
   poweredByHeader: false,
   output: "standalone",
 };
