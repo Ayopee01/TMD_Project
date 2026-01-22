e1934fcf3f8503716a323bf6a352a4baaf6a319c
Ayopee <Ayopee001@gmail.com>
2026-01-22 15:38:24 +0700

    update port3005

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ✅ เอาบรรทัดนี้กลับมาครับ! (เพื่อให้ App รู้จักคำว่า /test5)
-  basePath: '/test2', 
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   output: "standalone",
 };
 
